f619c52035b80ed90299dd91e49c9aabc7f3c4e2
Rezvan <rezvan.abasi555@gmail.com>
2022-05-31 17:11:18 -0700

    create model

--- virdaar/asgi.py
+++ virdaar/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Test project.
+ASGI config for virdaar project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Test.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'virdaar.settings')
 
 application = get_asgi_application()
